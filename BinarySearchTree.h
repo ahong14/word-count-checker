f81f4be6c07aaf2f34374eb794472e60615f71a3
Alex Raymond Hong <ahong14@pc10.cs.ucdavis.edu>
2017-09-19 00:38:58 -0700

    necessary files to run word count checker gen.out

---
+++ BinarySearchTree.h
@@ -0,0 +1,84 @@
+        #ifndef _BINARY_SEARCH_TREE_H_
+        #define _BINARY_SEARCH_TREE_H_
+
+        #include "dsexceptions.h"
+		#include "QueueLL.h"
+
+          // Binary node and forward declaration because g++ does
+          // not understand nested classes.
+        template <class Comparable>
+        class BinarySearchTree;
+
+        template <class Comparable>
+        class BinaryNode
+        {
+            Comparable element;
+            BinaryNode *left;
+            BinaryNode *right;
+            int count;//ADDED PARAMETER
+            void increment_count(); //ADDED FUNCTION
+            QueueLL<int> line;  //TEST LINE NUMBER
+            void add_line(int x);
+
+
+            //added variable
+            BinaryNode( const Comparable & theElement, BinaryNode *lt, BinaryNode *rt, int ct=0 )
+              : element( theElement ), left( lt ), right( rt ), count(ct)  { }
+            friend class BinarySearchTree<Comparable>;
+        };
+
+
+        // BinarySearchTree class
+        //
+        // CONSTRUCTION: with ITEM_NOT_FOUND object used to signal failed finds
+        //
+        // ******************PUBLIC OPERATIONS*********************
+        // void insert( x )       --> Insert x
+        // void remove( x )       --> Remove x
+        // Comparable find( x )   --> Return item that matches x
+        // Comparable findMin( )  --> Return smallest item
+        // Comparable findMax( )  --> Return largest item
+        // boolean isEmpty( )     --> Return true if empty; else false
+        // void makeEmpty( )      --> Remove all items
+        // void printTree( )      --> Print tree in sorted order
+
+        template <class Comparable>
+        class BinarySearchTree
+        {
+          public:
+            explicit BinarySearchTree( const Comparable & notFound );
+            BinarySearchTree( const BinarySearchTree & rhs );
+            ~BinarySearchTree( );
+
+            const Comparable & findMin( ) const;
+            const Comparable & findMax( ) const;
+            const Comparable & find( const Comparable & x ) const;
+          //  void find(const Comparable & x) const;
+            bool isEmpty( ) const;
+            void printTree( ) const;
+
+            void makeEmpty( );
+            void insert( const Comparable & x, int l );
+            void remove( const Comparable & x );
+
+
+            const BinarySearchTree & operator=( const BinarySearchTree & rhs );
+
+          private:
+            BinaryNode<Comparable> *root;
+            const Comparable ITEM_NOT_FOUND;
+
+            const Comparable & elementAt( BinaryNode<Comparable> *t ) const;
+
+            void insert( const Comparable & x, BinaryNode<Comparable> * & t, int l ) const;
+            void remove( const Comparable & x, BinaryNode<Comparable> * & t ) const;
+            BinaryNode<Comparable> * findMin( BinaryNode<Comparable> *t ) const;
+            BinaryNode<Comparable> * findMax( BinaryNode<Comparable> *t ) const;
+            BinaryNode<Comparable> * find( const Comparable & x, BinaryNode<Comparable> *t ) const;
+            void makeEmpty( BinaryNode<Comparable> * & t ) const;
+            void printTree( BinaryNode<Comparable> *t ) const;
+            BinaryNode<Comparable> * clone( BinaryNode<Comparable> *t ) const;
+        };
+
+        #include "BinarySearchTree.cpp"
+        #endif
---
+++ gen.cpp
@@ -0,0 +1,93 @@
+//============================================================================
+// Name        : p3.cpp
+// Author      : Alex Hong
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include<fstream>
+#include<cstring>
+#include<iomanip>
+#include<cctype>
+#include "BinarySearchTree.h"
+//#include "BTree.h"
+//#include "SeparateChaining.h"
+//#include "QuadraticProbing.h"
+//#include "QuadraticProbingPtr.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	ifstream inf;
+
+	//command line prompt
+	inf.open(argv[1]);
+
+//	inf.open("getty.txt");
+
+	if (inf.fail()) {
+		cout << "file failed to open" << endl;
+	}
+
+	//read in words from file, modify cstring
+	char word[30];
+	int length = 0;
+	char temp;
+	int line_number = 1;
+
+	BinarySearchTree<char*> bst(NULL);
+
+	while (!inf.eof()) {
+		if (temp == '\n') {
+			line_number++;
+		}
+		inf.get(temp);
+
+		if (isalpha(temp)) {
+			while (isalpha(temp)) {
+				if (isupper(temp)) {
+					temp = temp + 32;
+				}
+				word[length++] = temp;
+				inf.get(temp);
+
+			}
+
+//			if (temp == '\n') {
+//				line_number++;
+//			}
+
+			word[length] = '\0';
+
+			length = 0;
+
+			char* test = new char[30];
+			strcpy(test, word);
+
+			bst.insert(test, line_number);
+		}
+
+	}
+
+	bst.printTree();
+
+//single word
+//	inf.get(temp);
+//	while (isalpha(temp)) {
+//		if(isupper(temp))
+//		{
+//			temp=temp+32;
+//		}
+//		word[length++] = temp;
+//		inf.get(temp);
+//
+//	}
+//	//length--;
+//	word[length] = '\0';
+
+	return 0;
+}
+
